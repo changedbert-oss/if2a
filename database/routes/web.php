990140e63a8d1ad7a811d3f5730c5eb035c24090
changedbert-oss <changedbert@gmail.com>
2026-04-30 14:59:24 +0700

    Update web.php

--- database/routes/web.php
+++ database/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\FakultasController;
+use App\Http\Controllers\ProdiController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -13,4 +14,4 @@ Route::get('/tentang', function () {
 
 Route::resource('/Fakultas', FakultasController::class);
 
-route::resource('/periode', FakultasController::class);
+route::resource('/prodi', ProdiController::class, 'index');
